b6702a98a38d59ad0c8dec6a5f0ee22f823b48d8
Sakharchuk Evgeniya <janeangel0k@ya.ru>
2023-11-28 22:09:58 +0300

    commit_paramTest_forFAQ

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -4,189 +4,61 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
 
 public class MainPage {
-    private static final String site = "https://qa-scooter.praktikum-services.ru/";
 
-    public void openSite() {
-        driver.get(site);
-    }
-    // открытие главной страницы
+
+    // url главной страницы-----------------------------------------------------------
+    private final String site = "https://qa-scooter.praktikum-services.ru/";
+    public void openSite() {driver.get(site); }
+    //--------------------------------------------------------------------------------------
     public static By orderButtonAbove = By.xpath(".//button[@class='Button_Button__ra12g']");
-    // кнопка Заказать в шапке
+    // кнопка Заказать в шапке сайта
     public static By orderButtonCenter = By.xpath(".//button[contains(@class, 'Button_Middle__1CSJM')]");
-    // кнопка заказать в центре
-    private final By questionImportant = By.xpath(".//div[text()='Вопросы о важном']");
-    // заголовок Вопросы о важном
+    // кнопка заказать по центру сайта
     private final By cookieButton = By.id("rcc-confirm-button");
-    // кнопка куки
+    // кнопка с куки
+    private final By frequentlyAskedQuestionsHeader = By.xpath("//div[@class='Home_FourPart__1uthg']//div[@class='Home_SubHeader__zwi_E']");
+// Заголовок Вопросы о важном new
 
-    //------------ Блок Вопросы -----------------
-    public static By questionOne = By.id("accordion__heading-0");
-    //вопрос №1 стоимость и оплата
-    public static By questionTwo = By.id("accordion__heading-1");
-    //вопрос №2 заказ нескольких самокатов
-    public static By questionThree = By.id("accordion__heading-2");
-    //вопрос №3 расчет времени аренды
-    public static By questionFour = By.id("accordion__heading-3");
-    //вопрос №4 заказ самоката на сегодня
-    public static By questionFive = By.id("accordion__heading-4");
-    //вопрос №5 продление или возврат раньше срока
-    public static By questionSix = By.id("accordion__heading-5");
-    //вопрос №6 зарядка с самокатом
-    public static By questionSeven = By.id("accordion__heading-6");
-    //вопрос №7 отмена заказа
-    public static By questionEight = By.id("accordion__heading-7");
-    //вопрос №8 доставка за МКАД
-
-    //------------- Ответы на вопросы -----------
-    public static By answerOne = By.id("accordion__panel-0");
-    //Ответ на вопрос №1
-    public static By answerTwo = By.id("accordion__panel-1");
-    //Ответ на вопрос №2
-    public static By answerThree = By.id("accordion__panel-2");
-    //Ответ на вопрос №3
-    public static By answerFour = By.id("accordion__panel-3");
-    //Ответ на вопрос №4
-    public static By answerFive = By.id("accordion__panel-4");
-    //Ответ на вопрос №5
-    public static By answerSix = By.id("accordion__panel-5");
-    //Ответ на вопрос №6
-    public static By answerSeven = By.id("accordion__panel-6");
-    //Ответ на вопрос №7
-    public static By answerEight = By.id("accordion__panel-7");
-    //Ответ на вопрос №8
-
-    //-----------Текстовки ответов -------------------------
-    public static String questionOneText = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
-    //Текст ответа на вопрос №1
-    public static String questionTwoText = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
-    //Текст ответа на вопрос №2
-    public static String questionThreeText = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
-    //Текст ответа на вопрос №3
-    public static String questionFourText = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
-    //Текст ответа на вопрос №4
-    public static String questionFiveText = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
-    //Текст ответа на вопрос №5
-    public static String questionSixText = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
-    //Текст ответа на вопрос №6
-    public static String questionSevenText = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
-    //Текст ответа на вопрос №7
-    public static String questionEightText = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
-    //Текст ответа на вопрос №8
-
+    //-----------------------------------------------------------------------------------
     private final WebDriver driver;
+    public MainPage(WebDriver driver) {this.driver = driver;}
 
-    public MainPage(WebDriver driver) {
-        this.driver = driver;
-    }
-
-    public void scrollToElement() {
-        WebElement element = driver.findElement(questionImportant);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
-        //скролл к блоку Вопросы о важном
-    }
+// Методы общие ----------------------------------------------------------
 
     public void clickCookie() {
         driver.findElement(cookieButton).click();
-        //принятие куки
-    }
 
+    }
+    //Методы заказа --------------------------------------------------------------
     public void clickOrderButton(By orderButton) {
         driver.findElement(orderButton).click();
-        //нажать Заказать
+
     }
 
-    public void clickButtonOneQuestion(By questionOne) {
-        driver.findElement(questionOne).click();
-        //нажать на вопрос 1
+    //Методы для вопрос-ответ--------------------------------------------------
+    public void goToFAQ() {
+        WebElement faqHeader = driver.findElement(frequentlyAskedQuestionsHeader);
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].scrollIntoView();", faqHeader);//проскролить до заголовка Вопросы о важном new
     }
 
-    public String getTextQuestionOne() {
-        driver.findElement(answerOne).getText();
-        return questionOneText;
-        //ответ на вопрос 1
+    public void openAccordionQuestion(String question) {
+        driver.findElement(By.xpath("//div[@class='accordion__heading']//div[text()='" + question + "']")).click();
     }
 
-    public void clickButtonTwoQuestion(By questionTwo) {
-        driver.findElement(questionTwo).click();
-        //нажать на вопрос 2
-    }
-
-    public String getTextQuestionTwo() {
-        driver.findElement(answerTwo).getText();
-        return questionTwoText;
-        //ответ на вопрос 2
-    }
-
-    public void clickButtonThreeQuestion(By questionThree) {
-        driver.findElement(questionThree).click();
-        //нажать на вопрос 3
-    }
-
-    public String getTextQuestionThree() {
-        driver.findElement(answerThree).getText();
-        return questionThreeText;
-        //ответ на вопрос 3
-    }
-
-    public void clickButtonFourQuestion(By questionFour) {
-        driver.findElement(questionFour).click();
-        //нажать на вопрос 4
-    }
-
-    public String getTextQuestionFour() {
-        driver.findElement(answerFour).getText();
-        return questionFourText;
-        //ответ на вопрос 4
-    }
-
-    public void clickButtonFiveQuestion(By questionFive) {
-        driver.findElement(questionFive).click();
-        //нажать на вопрос 5
-    }
-
-    public String getTextQuestionFive() {
-        driver.findElement(answerFive).getText();
-        return questionFiveText;
-        //ответ на вопрос 5
-    }
-
-    public void clickButtonSixQuestion(By questionSix) {
-        driver.findElement(questionSix).click();
-        //нажать на вопрос 6
-    }
-
-    public String getTextQuestionSix() {
-        driver.findElement(answerSix).getText();
-        return questionSixText;
-        //ответ на вопрос 6
-    }
-
-    public void clickButtonSevenQuestion(By questionSeven) {
-        driver.findElement(questionSeven).click();
-        //нажать на вопрос 7
-    }
-
-    public String getTextQuestionSeven() {
-        driver.findElement(answerSeven).getText();
-        return questionSevenText;
-        //ответ на вопрос 7
-    }
-
-    public void clickButtonEightQuestion(By questionEight) {
-        driver.findElement(questionEight).click();
-        //нажать на вопрос 8
-    }
-
-    public String getTextQuestionEight() {
-        driver.findElement(answerEight).getText();
-        return questionEightText;
-        //ответ на вопрос 8
-    }
-
-    public void quitBrows(){
-        driver.quit();
+    public String getAnswer(String answer) {
+        final Wait<WebDriver> wait = new FluentWait<>(driver).withMessage("Элемент не найден").withTimeout(Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@class='accordion__panel']//p[text()='" + answer + "']"))));
+        return driver.findElement(By.xpath("//div[@class='accordion__panel']//p[text()='" + answer + "']")).getText();
     }
 
 }
+
--- src/test/java/checklistofquestion/CheckList.java
+++ src/test/java/checklistofquestion/CheckList.java
@@ -1,49 +1,65 @@
 package checklistofquestion;
-
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.By
-import static org.junit.Assert.assertEquals;
-import static pageobject.MainPage.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageobject.MainPage;
+import org.junit.Assert;
+import java.util.concurrent.TimeUnit;
 
-@RunWith(Parameterized.class)
-public class MainPageQuestions extends BaseTest {
-    static MainPage mainPage
-    private static String answer_text_expected;
-    private static By question_click;
-    private static By answer;
-//testdata
-    public MainPageQuestions(String answer_text_expected, By question_click, By answer) {
-        this.answer_text_expected = answer_text_expected;
-        this.question_click = question_click;
-        this.answer = answer;
+
+public class CheckListQuestion {
+    @RunWith(Parameterized.class)
+    public class ChecklistQuestion {
+        private WebDriver driver;
+        private final String question;
+        private final String answer;
+
+        public ChecklistQuestion(String question, String answer) {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] getQuestions() {
+            return new Object[][]{
+                    {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                    {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                    {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                    {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                    {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                    {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                    {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                    {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+            };
+        }
+
+        @Before
+        public void setUp() {
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            MainPage mainPage = new MainPage(driver);
+            mainPage.openSite();
+            mainPage.clickCookie();
+        }
+
+        @Test
+        public void ListTest() {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.goToFAQ();
+            mainPage.openAccordionQuestion(question);
+            String reply = mainPage.getAnswer(answer);
+            Assert.assertEquals(answer, reply);
+        }
+
+        @After
+        public void tearDown() {
+            driver.quit();
+        }
     }
-
-    @Parameterized.Parameters
-
-    public static Object[][] getCredentials() {
-        return new Object[][] {
-                { questionOneText,questionOne ,answerOne},
-                { questionTwoText, questionTwo, answerTwo},
-                { questionThreeText, questionThree, answerThree},
-                { questionFourText, questionFour, answerFour},
-                { questionFiveText, questionFive, answerFive},
-                { questionSixText, questionSix, answerSix},
-                { questionSevenText, questionSeven, answerSeven},
-                { questionEightText, questionEight, answerEight}
-        };
-    }
-
-    @Test
-    public void checkActualText_expectText(){
-        super.implicitlyWait(10);
-        MainPage mainPage = new MainPage(driver);
-        mainPage.Open();
-        String actualQuestion = mainPage.clickQuest(question_click);
-        String actualAnswer = mainPage.answerDisplayed(answer);
-        assertEquals(answer_text_expected, question_click, answer);
-    }
-
 }
+
+
